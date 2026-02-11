63f6c8c37252a2ad94ac7d177e990e832557b0cb
briaaaaann <123046511@upq.edu.mx>
2026-02-10 23:41:43 -0600

    implementacion de Endpoints con parametros obligatorios y no opcionales

--- miAPI/main.py
+++ miAPI/main.py
@@ -1,6 +1,8 @@
 #Importaciones
 from fastapi import FastAPI
+from typing import Optional
 import asyncio
+
 
 #Instancia del servidor
 app = FastAPI()
@@ -17,3 +19,17 @@ async def bienvenido():
         "mensaje":"Bienvenido a FastAPI",
         "estatus":"200"
         }
+
+#Endpoint con Parametros obligatorios
+@app.get("/saludo/{nombre}")
+async def saludo(nombre: str):
+    return{"mensdaje": f"Hola, {nombre}. Este es un parametro obligatorio"}
+
+#Endpoint con Parametros opcionales
+@app.get("/busqueda")
+async def buscar(q: Optional[str] = None):
+    if q:
+        return{"resultado" : f"Estas buscando: {q}"}
+    else:
+        return{"resultado" : "No buscaste nada"}
+
